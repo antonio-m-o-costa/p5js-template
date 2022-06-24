af7a510beb0d3622727825d323375a824f2a11f0
António Costa <93203571+antonio-m-o-costa@users.noreply.github.com>
2022-06-24 15:17:03 +0100

    Cosmetics added

--- sketch.js
+++ sketch.js
@@ -1,8 +1,20 @@
+/* Canvas sizes I love to use:
+ * 640x480 - (4:3)
+ * 1280x720 - (16:9)
+ */
+
 function setup() {
-    let canvas = createCanvas(400, 400);
+    let canvas = createCanvas(640, 480);
     canvas.parent("sketch");
 }
 
 function draw() {
-    background(220);
+    background(0);
+    stroke(255);
+    strokeWeight(2);
+    for (let i = 10; i < width - 9; i += 10) {
+        for (let j = 10; j < height - 9; j += 10) {
+            point(i, j);
+        }
+    }
 }
